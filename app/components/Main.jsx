d5ea12cbc80e5718089cc721a3650916d25ebd2c
Hyungju Kwon <hyungju.kwon@edelman.com>
2016-08-29 19:59:38 -0400

    Added navigation

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,18 +1,17 @@
 var React = require('react');
 var Navigation = require('Navigation');
 
-var Main = React.createClass({
-	render: function () {
-		return (
+var Main = (props) => {
+	return (
+		<div>
+			<Navigation />
 			<div>
-				<Navigation />
-				<div>
-					<p>LOL</p>
-				</div>
+				<p>LOL</p>
+				{props.children}
 			</div>
-		)
-	}
-});
+		</div>
+	)
+};
 
 
 module.exports = Main;
