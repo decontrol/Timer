02f582eb4b3c1dd0230c3051b38b0abad4362c55
Hyungju Kwon <hyungju.kwon@edelman.com>
2016-09-16 16:24:18 -0400

    CoundownForm + test

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,9 +5,10 @@ var Main = (props) => {
 	return (
 		<div>
 			<Navigation />
-			<div>
-				<p>LOL</p>
-				{props.children}
+			<div className="row">
+				<div className="column small-centered medium-6 large-4">
+					{props.children}
+				</div>
 			</div>
 		</div>
 	)
